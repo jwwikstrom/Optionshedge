bf1ede4f077e944cc891375fc0607900d738406a
Mandelius <marma148@student.liu.se>
2016-12-13 16:49:18 +0100

    Principalcomp
    Har lyckats få till sorteringen men inte nöjd med egenvektorerna

--- prinComp.m
+++ prinComp.m
@@ -3,22 +3,33 @@ function [ output_args ] = prinComp( input_args )
 % 99 (?) percent of the variance
 %   Detailed explanation goes here
 
-data2 = flipud(xlsread('data.xlsx','PCA','C4:IF845'));
-funds = data;
-faktorData = data2;
+volsurfaces= flipud(xlsread('data.xlsx','PCA','C4:IF845'));
+temp = zeros(14,17,842);
+% 14 maturities x 17 deltas x 842 dates
+for k = 1:842
 
-fundReturns = log(funds(2:length(funds))./funds(1:length(funds)-1));
+temp(:,:,k) = reshape(volsurfaces(k,:),[17,14])';    
+end
+
+
+% 14 maturities x 17 deltas x 841 returns
+volDiff = log(temp(:,:,2:end)./temp(:,:,1:end-1));
+
+% (14 mat x 17 delta) x 841 returns
+for i = 1:841
+    volDiff2D(:,i) = reshape(volDiff(:,:,i),[238,1]);
+end
+
 
 
 %% Create the eigen vectors
-indexReturns = log(faktorData(2:length(faktorData),:)./faktorData(1:length(faktorData)-1,:));
 
-[eigVec, eigValD]=eig(cov(indexReturns));
+[eigVec, eigValD]=eig(cov(volDiff2D'));
 
 
 %% Choose the k components that describe target range of variance
 
-target = 0.99;
+target = 0.95;
 eigVal = flipud(diag(eigValD));
 varExpl = 0;
 k=0;
@@ -29,14 +40,14 @@ end
 
 pComp = eigVec(:,1:k);
 
-B = pComp;
+B = reshape(pComp,[14,17,k]);
 
-for j = 1:length(indexReturns)
-    for i = 1:k
-        F(j,i)=indexReturns(j,:)*B(:,i);
-    end
-end
-ctrl1 = cov(F);
+% for j = 1:length(volDiff2D)
+%     for i = 1:k
+%         F(j,i)=indexReturns(j,:)*B(:,i);
+%     end
+% end
+% ctrl1 = cov(F);
 
 end
 
